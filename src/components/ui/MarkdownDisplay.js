cbb888b74b85a8913ee1722d5d83647127498ef4
sd0e <sebastiandoe5@gmail.com>
2022-12-12 22:04:55 +0000

    Introduce LaTeX

--- src/components/ui/MarkdownDisplay.js
+++ src/components/ui/MarkdownDisplay.js
@@ -3,6 +3,9 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import RemarkMathPlugin from 'remark-math';
+import { BlockMath, InlineMath } from 'react-katex';
+import '../../../node_modules/katex/dist/katex.min.css';
 import { IconButton, createTheme, ThemeProvider } from '@mui/material';
 import { ContentCopy } from '@mui/icons-material';
 
@@ -41,7 +44,7 @@ export default function MarkdownDisplay({ StoryContent }) {
 	return (
 		<ReactMarkdown
 			children={StoryContent}
-			remarkPlugins={[ remarkGfm ]}
+			remarkPlugins={[ remarkGfm, RemarkMathPlugin ]}
 			className={classes.markdownDisplay}
 			components={{
 				code({ node, inline, className, children, ...props }) {
@@ -76,6 +79,10 @@ export default function MarkdownDisplay({ StoryContent }) {
 						if (children[0].includes('|||')) {
 							children = children[0].split('|||');
 							return <Information Title={children[0]} Article>{children[1]}</Information>
+						} else if (children.includes('$$') && !children.includes('\\$\\$')) {
+							return <BlockMath children={children} />;
+						} else if (children.includes('$') && !children.includes('\\$')) {
+							return <InlineMath children={children} />;
 						} else return <p {...props}>{children}</p>;
 					}
 					else return <p {...props}>{children}</p>;
